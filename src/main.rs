4838202f5dd2040f1eecef4a929e28174106aa14
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-08-01 16:54:55 +0200

    feat: add read file function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{io::{self, Error}, path::Path};
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn read_file(file_path: &str) -> Result<String, Error> {
+
+    let path = Path::new(&file_path);
+
+    if !path.exists(){
+        return Err(io::Error::new(io::ErrorKind::NotFound, "File doesn't exist"));
+    }
+
+
+    std::fs::read_to_string(file_path)
 }
