1d1afaa75f5f165f7e797f1c30a9ee4e302d695c
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-08-04 20:46:07 +0200

    refactor: palette refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{self, Error},
     path::Path,
@@ -7,60 +8,62 @@ use std::{
 #[derive(Debug)]
 struct Palette {
     name: String,
-    base03: String,
-    base02: String,
-    base01: String,
-    base00: String,
-    base1: String,
-    base2: String,
-    base3: String,
-    yellow: String,
-    orange: String,
-    red: String,
-    magenta: String,
-    violet: String,
-    blue: String,
-    cyan: String,
-    green: String,
+    colors: HashMap<String, String>,
 }
 
 fn main() {
-    let _solarized = Palette {
-        name: "Solarized".to_string(),
-        base03: "#002b36".to_string(),
-        base02: "#073642".to_string(),
-        base01: "#586e75".to_string(),
-        base00: "#657b83".to_string(),
-        base1: "#839496".to_string(),
-        base2: "#93a1a1".to_string(),
-        base3: "#fdf6e3".to_string(),
-        yellow: "#b58900".to_string(),
-        orange: "#cb4b16".to_string(),
-        red: "#dc322f".to_string(),
-        magenta: "#d33682".to_string(),
-        violet: "#6c71c4".to_string(),
-        blue: "#268bd2".to_string(),
-        cyan: "#2aa198".to_string(),
-        green: "#859900".to_string(),
+    let solarized = Palette {
+        name: "Solarized Osaka".to_string(),
+        colors: HashMap::from([
+            ("base03".to_string(), "#001419".to_string()),
+            ("base02".to_string(), "#002b36".to_string()),
+            ("base01".to_string(), "#586e75".to_string()),
+            ("base00".to_string(), "#657b83".to_string()),
+            ("base0".to_string(), "#839496".to_string()),
+            ("base1".to_string(), "#93a1a1".to_string()),
+            ("base2".to_string(), "#eee8d5".to_string()),
+            ("base3".to_string(), "#fdf6e3".to_string()),
+            ("yellow".to_string(), "#b58900".to_string()),
+            ("orange".to_string(), "#cb4b16".to_string()),
+            ("red".to_string(), "#dc322f".to_string()),
+            ("magenta".to_string(), "#d33682".to_string()),
+            ("violet".to_string(), "#6c71c4".to_string()),
+            ("blue".to_string(), "#268bd2".to_string()),
+            ("cyan".to_string(), "#2aa198".to_string()),
+            ("green".to_string(), "#859900".to_string()),
+        ]),
     };
 
     let catppuccin = Palette {
         name: "Catppuccin Mocha".to_string(),
-        base03: "#1e1e2e".to_string(), 
-        base02: "#181825".to_string(), 
-        base01: "#313244".to_string(), 
-        base00: "#45475a".to_string(), 
-        base1: "#585b70".to_string(),  
-        base2: "#6c7086".to_string(),  
-        base3: "#cdd6f4".to_string(),  
-        yellow: "#f9e2af".to_string(),
-        orange: "#fab387".to_string(),
-        red: "#f38ba8".to_string(),
-        magenta: "#f5c2e7".to_string(),
-        violet: "#cba6f7".to_string(),
-        blue: "#89b4fa".to_string(),
-        cyan: "#94e2d5".to_string(),
-        green: "#a6e3a1".to_string(),
+        colors: HashMap::from([
+            ("rosewater".to_string(), "#f5e0dc".to_string()),
+            ("flamingo".to_string(), "#f2cdcd".to_string()),
+            ("pink".to_string(), "#f5c2e7".to_string()),
+            ("mauve".to_string(), "#cba6f7".to_string()),
+            ("red".to_string(), "#f38ba8".to_string()),
+            ("maroon".to_string(), "#eba0ac".to_string()),
+            ("peach".to_string(), "#fab387".to_string()),
+            ("yellow".to_string(), "#f9e2af".to_string()),
+            ("green".to_string(), "#a6e3a1".to_string()),
+            ("teal".to_string(), "#94e2d5".to_string()),
+            ("sky".to_string(), "#89dceb".to_string()),
+            ("sapphire".to_string(), "#74c7ec".to_string()),
+            ("blue".to_string(), "#89b4fa".to_string()),
+            ("lavender".to_string(), "#b4befe".to_string()),
+            ("text".to_string(), "#cdd6f4".to_string()),
+            ("subtext1".to_string(), "#bac2de".to_string()),
+            ("subtext0".to_string(), "#a6adc8".to_string()),
+            ("overlay2".to_string(), "#9399b2".to_string()),
+            ("overlay1".to_string(), "#7f849c".to_string()),
+            ("overlay0".to_string(), "#6c7086".to_string()),
+            ("surface2".to_string(), "#585b70".to_string()),
+            ("surface1".to_string(), "#45475a".to_string()),
+            ("surface0".to_string(), "#313244".to_string()),
+            ("base".to_string(), "#1e1e2e".to_string()),
+            ("mantle".to_string(), "#181825".to_string()),
+            ("crust".to_string(), "#11111b".to_string()),
+        ]),
     };
 }
 
@@ -83,4 +86,8 @@ fn write_file(file_path: &str, contents: &str) {
     }
 
     std::fs::write(file_path, contents).expect("Cannot write to file")
+}
+
+fn replace_colors(from_palette: Palette, to_palette: Palette, content: String) -> String {
+    todo!()
 }
