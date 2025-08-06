4cc3ab118dc863bc783883e683305765fd0ac9f8
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-08-06 20:35:58 +0200

    feat: add very messy test

--- src/main.rs
+++ src/main.rs
@@ -39,36 +39,48 @@ fn main() {
     let catppuccin = Palette {
         name: "Catppuccin Mocha".to_string(),
         colors: HashMap::from([
-            ("rosewater".to_string(), "#f5e0dc".to_string()),
-            ("flamingo".to_string(), "#f2cdcd".to_string()),
-            ("pink".to_string(), "#f5c2e7".to_string()),
-            ("mauve".to_string(), "#cba6f7".to_string()),
-            ("red".to_string(), "#f38ba8".to_string()),
-            ("maroon".to_string(), "#eba0ac".to_string()),
-            ("peach".to_string(), "#fab387".to_string()),
+            ("base03".to_string(), "#11111b".to_string()),
+            ("base02".to_string(), "#181825".to_string()),
+            ("base01".to_string(), "#313244".to_string()),
+            ("base00".to_string(), "#45475a".to_string()),
+            ("base0".to_string(), "#cdd6f4".to_string()),
+            ("base1".to_string(), "#bac2de".to_string()),
+            ("base2".to_string(), "#f5e0dc".to_string()),
+            ("base3".to_string(), "#f2cdcd".to_string()),
             ("yellow".to_string(), "#f9e2af".to_string()),
-            ("green".to_string(), "#a6e3a1".to_string()),
-            ("teal".to_string(), "#94e2d5".to_string()),
-            ("sky".to_string(), "#89dceb".to_string()),
-            ("sapphire".to_string(), "#74c7ec".to_string()),
+            ("orange".to_string(), "#fab387".to_string()),
+            ("red".to_string(), "#f38ba8".to_string()),
+            ("magenta".to_string(), "#f5c2e7".to_string()),
+            ("violet".to_string(), "#cba6f7".to_string()),
             ("blue".to_string(), "#89b4fa".to_string()),
-            ("lavender".to_string(), "#b4befe".to_string()),
-            ("text".to_string(), "#cdd6f4".to_string()),
-            ("subtext1".to_string(), "#bac2de".to_string()),
-            ("subtext0".to_string(), "#a6adc8".to_string()),
-            ("overlay2".to_string(), "#9399b2".to_string()),
-            ("overlay1".to_string(), "#7f849c".to_string()),
-            ("overlay0".to_string(), "#6c7086".to_string()),
-            ("surface2".to_string(), "#585b70".to_string()),
-            ("surface1".to_string(), "#45475a".to_string()),
-            ("surface0".to_string(), "#313244".to_string()),
-            ("base".to_string(), "#1e1e2e".to_string()),
-            ("mantle".to_string(), "#181825".to_string()),
-            ("crust".to_string(), "#11111b".to_string()),
+            ("cyan".to_string(), "#94e2d5".to_string()),
+            ("green".to_string(), "#a6e3a1".to_string()),
         ]),
     };
 
-    //replace_colors(solarized, catppuccin);
+    let content = String::from("[colors]
+base03 = #11111b
+base02 = #181825
+base01 = #313244
+base00 = #45475a
+base0  = #cdd6f4
+base1  = #bac2de
+base2  = #f5e0dc
+base3  = #f2cdcd
+
+yellow   = #f9e2af
+orange   = #fab387
+red      = #f38ba8
+magenta  = #f5c2e7
+violet   = #cba6f7
+blue     = #89b4fa
+cyan     = #94e2d5
+green    = #a6e3a1
+");
+
+    let new = replace_colors(catppuccin, solarized, content);
+
+    println!("{new}");
 }
 
 fn read_file(file_path: &str) -> Result<String, Error> {
@@ -101,7 +113,7 @@ fn replace_colors(from_palette: Palette, to_palette: Palette, content: String) -
             if name == to_name {
                 let str = format!("(?i){}", regex::escape(hex));
                 let re = Regex::new(&str).unwrap();
-                result = re.replace_all(&result, "p").into_owned();
+                result = re.replace_all(&result, to_hex).into_owned();
 
                 println!("Replaced: {name}{hex} →  {to_hex}");
             }
