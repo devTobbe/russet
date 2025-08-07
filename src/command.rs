9313994b3b81493c54d4ecf50a0bee09e222b5b2
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-08-07 18:22:28 +0200

    feat: add available commands

---
+++ src/command.rs
@@ -0,0 +1,23 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(name = "TEST")]
+#[command(about = "A palette swapping cli application" , long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    /// Convert a file from one palette to another, available in verison one
+    Convert { from: String, to: String },
+    /// List the currently available palettes
+    List,
+    /// Add a palette
+    Add {name : String, contents : String},
+    /// Delete a palette
+    Delete {name : String},
+    /// Edit a palette
+    Edit {name : String, contents: String},
+}
