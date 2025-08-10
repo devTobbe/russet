c039867e0968a1bf13f7c33104173d3499986444
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-08-10 15:42:42 +0200

    feat: add error propagation to palette_utils

--- src/palette_utils.rs
+++ src/palette_utils.rs
@@ -1,20 +1,24 @@
-use regex::Regex;
+use regex::{Error, Regex};
 
 use crate::palette::Palette;
 
-fn replace_colors(from_palette: Palette, to_palette: Palette, content: String) -> String {
+fn replace_colors(
+    from_palette: Palette,
+    to_palette: Palette,
+    content: String,
+) -> Result<String, Error> {
     let mut result = content;
 
     for (name, hex) in &from_palette.colors {
         for (to_name, to_hex) in &to_palette.colors {
             if name == to_name {
                 let str = format!("(?i){}", regex::escape(hex));
-                let re = Regex::new(&str).unwrap();
+                let re = Regex::new(&str)?;
                 result = re.replace_all(&result, to_hex).into_owned();
 
                 println!("Replaced: {name}{hex} →  {to_hex}");
             }
         }
     }
-    result
+    Ok(result)
 }
