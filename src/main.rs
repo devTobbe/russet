ec11cc569739830241e27782b2d62e74c07cb061
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-08-12 20:46:45 +0200

    feat: add handle_convert
    handles the convert cli option and the main functionality of the
    applicatio

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use clap::Parser;
 
 use crate::command::{Cli, Command};
 
+use crate::palette_utils::replace_colors;
 use crate::storage::{deserialize_palettes, read_file, serialize_palettes, write_file};
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -16,18 +17,45 @@ fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
 
     match &cli.command {
-        Command::Convert { from, to } => {
-            todo!()
-        }
+        Command::Convert { from, to, file } => handle_convert(from, to, file),
         Command::List => handle_list(),
         Command::Add { name, contents } => {
             todo!()
         }
-        Command::Delete { name } => {handle_delete(name)}
+        Command::Delete { name } => handle_delete(name),
         Command::Edit { name, contents } => {
             todo!()
         }
     }
+}
+
+fn handle_convert(from: &str, to: &str, file: &str) -> Result<(), Box<dyn Error>> {
+    // Fetch available palettes
+    let s = read_file("palettes.toml")?;
+    let palettes = deserialize_palettes(&s)?;
+
+    // Fetch the file to convert
+    let conv_file = read_file(file)?;
+
+    // Fetch the two palettes
+    let from_p = palettes
+        .palette_collection
+        .iter()
+        .find(|p| p.name == from)
+        .ok_or_else(|| format!("Couldn't find a palette with name: {}", from))?;
+    let to_p = palettes
+        .palette_collection
+        .iter()
+        .find(|p| p.name == to)
+        .ok_or_else(|| format!("Couldn't find a palette with name: {}", to))?;
+
+    let new_file = replace_colors(from_p, to_p, &conv_file)?;
+
+    // FIX: Doesn't handle the file extension atm. So name will be something like
+    // theme2.toml_solarized
+    let new_file_name = format!("{}_{}", file, to);
+    write_file(&new_file_name, &new_file)?;
+    Ok(())
 }
 
 fn handle_list() -> Result<(), Box<dyn Error>> {
