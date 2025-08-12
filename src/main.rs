811d734325a29e40ec9a85070ee6ee186eb64a1a
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-08-12 20:01:44 +0200

    feat: add handle_delete
    added functionality to be able to handle the delete case

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use clap::Parser;
 
 use crate::command::{Cli, Command};
 
-use crate::storage::{deserialize_palettes, read_file};
+use crate::storage::{deserialize_palettes, read_file, serialize_palettes, write_file};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let file = read_file("");
@@ -23,9 +23,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         Command::Add { name, contents } => {
             todo!()
         }
-        Command::Delete { name } => {
-            todo!()
-        }
+        Command::Delete { name } => {handle_delete(name)}
         Command::Edit { name, contents } => {
             todo!()
         }
@@ -36,5 +34,15 @@ fn handle_list() -> Result<(), Box<dyn Error>> {
     let s = read_file("palettes.toml")?;
     let palettes = deserialize_palettes(&s)?;
     println!("{palettes:?}");
+    Ok(())
+}
+
+// FIX: Duplicate names is unhandled right now, remeber to handle these cases later on.
+fn handle_delete(del_name: &str) -> Result<(), Box<dyn Error>> {
+    let s = read_file("palettes.toml")?;
+    let mut palettes = deserialize_palettes(&s)?;
+    palettes.palette_collection.retain(|p| p.name != del_name);
+    let deser_p = serialize_palettes(&palettes)?;
+    write_file("palettes.toml", &deser_p)?;
     Ok(())
 }
