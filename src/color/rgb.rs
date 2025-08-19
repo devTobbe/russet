8dcc1c3a98dbdbc272593b58d0e2a7eec68bfecd
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-08-19 20:32:08 +0200

    refactor: rename color_spaces

---
+++ src/color/rgb.rs
@@ -0,0 +1,30 @@
+use core::fmt;
+use std::error::Error;
+
+#[derive(Debug)]
+struct Rgb {
+    pub r: u8, // 0-255
+    pub g: u8, // 0-255
+    pub b: u8, // 0-255
+}
+
+impl Rgb {
+    fn new(r: u8, g: u8, b: u8) -> Self {
+        Self { r, g, b }
+    }
+
+    fn to_hex(&self) -> Result<String, Box<dyn Error>> {
+        todo!()
+    }
+
+    fn from_hex(s: &str) -> Result<Rgb, Box<dyn Error>> {
+        // TODO: Set as self maybe? Has to be mut then...
+        todo!()
+    }
+}
+
+impl fmt::Display for Rgb {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "RGB({}, {}, {})", self.r, self.g, self.b)
+    }
+}
