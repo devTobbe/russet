2f0178b0a64c4495008d3b68e25cdf9e8bf97752
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-08-25 20:50:03 +0200

    feat: add from_hex

--- src/color/rgb.rs
+++ src/color/rgb.rs
@@ -18,13 +18,36 @@ impl Rgb {
     }
 
     fn from_hex(s: &str) -> Result<Rgb, Box<dyn Error>> {
-        // TODO: Set as self maybe? Has to be mut then...
-        todo!()
+        // TODO: ADD ERROR CHECKING ON LENGTH
+        let s = s.trim_start_matches('#');
+        let r_h = &s[0..2];
+        let g_h = &s[2..4];
+        let b_h = &s[4..6];
+
+        let r = u8::from_str_radix(r_h, 16)?;
+        let g = u8::from_str_radix(g_h, 16)?;
+        let b = u8::from_str_radix(b_h, 16)?;
+
+        Ok(Self { r, g, b })
     }
 }
 
 impl fmt::Display for Rgb {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "RGB({}, {}, {})", self.r, self.g, self.b)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let s = "#001419";
+        let from_hex = Rgb::from_hex(s).unwrap();
+        let test = Rgb::new(0, 20, 25);
+
+        assert_eq!(from_hex, test);
     }
 }
