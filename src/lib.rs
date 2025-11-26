5d4a26eba582d421d930903ad123e074938308d9
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-11-26 20:50:44 +0100

    chore: clean up

--- src/lib.rs
+++ src/lib.rs
@@ -35,35 +35,6 @@ pub fn run() -> Result<(), Box<dyn Error>> {
     }
 }
 
-//fn handle_convert(from: &str, to: &str, file: &str) -> Result<(), Box<dyn Error>> {
-//    // Fetch available palettes
-//    let s = read_file("palettes.toml")?;
-//    let palettes = deserialize_palettes(&s)?;
-//
-//    // Fetch the file to convert
-//    let conv_file = read_file(file)?;
-//
-//    // Fetch the two palettes
-//    let from_p = palettes
-//        .palette_collection
-//        .iter()
-//        .find(|p| p.name == from)
-//        .ok_or_else(|| format!("Couldn't find a palette with name: {}", from))?;
-//    let to_p = palettes
-//        .palette_collection
-//        .iter()
-//        .find(|p| p.name == to)
-//        .ok_or_else(|| format!("Couldn't find a palette with name: {}", to))?;
-//
-//    let new_file = replace_colors(from_p, to_p, &conv_file)?;
-//
-//    // FIX: Doesn't handle the file extension atm. So name will be something like
-//    // theme2.toml_solarized
-//    let new_file_name = format!("{}_{}", file, to);
-//    write_file(&new_file_name, &new_file)?;
-//    Ok(())
-//}
-
 fn handle_list() -> Result<(), Box<dyn Error>> {
     let s = read_file("palettes.toml")?;
     let palettes = deserialize_palettes(&s)?;
